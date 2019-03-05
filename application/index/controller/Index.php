e61bf95d1f8ffd335c341d83e5b2274d5bbdae1c
qianchao Mac <qianchaoMac@qq.com>
2019-03-05 23:49:22 +0800

    阿里云人脸识别集成

---
+++ application/index/controller/Index.php
@@ -0,0 +1,62 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        $akId = "";
+        $akSecret = "";
+
+        $url = "https://dtplus-cn-shanghai.data.aliyuncs.com/face/attribute";
+
+        $content_arr['type'] = 0;
+        $content_arr['image_url'] = '';
+        $content = json_encode($content_arr);
+
+        $options = array(
+            'http' => array(
+                'header' => array(
+                    'accept'=> "application/json",
+                    'content-type'=> "application/json",
+                    'date'=> gmdate("D, d M Y H:i:s \G\M\T"),
+                    'authorization' => ''
+                ),
+                'method' => "POST", //可以是 GET, POST, DELETE, PUT
+                'content' => $content, //如有数据，请用json_encode()进行编码
+            )
+        );
+        $http = $options['http'];
+        $header = $http['header'];
+        $urlObj = parse_url($url);
+        if(empty($urlObj["query"]))
+            $path = $urlObj["path"];
+        else
+            $path = $urlObj["path"]."?".$urlObj["query"];
+        $body = $http['content'];
+        if(empty($body))
+            $bodymd5 = $body;
+        else
+            $bodymd5 = base64_encode(md5($body,true));
+        $stringToSign = $http['method']."\n".$header['accept']."\n".$bodymd5."\n".$header['content-type']."\n".$header['date']."\n".$path;
+        $signature = base64_encode(
+            hash_hmac(
+                "sha1",
+                $stringToSign,
+                $akSecret, true));
+        $authHeader = "Dataplus "."$akId".":"."$signature";
+        $options['http']['header']['authorization'] = $authHeader;
+        $options['http']['header'] = implode(
+            array_map(
+                function($key, $val){
+                    return $key.":".$val."\r\n";
+                },
+                array_keys($options['http']['header']),
+                $options['http']['header']));
+        $context = stream_context_create($options);
+        $file = file_get_contents($url, false, $context );
+
+        $file = json_decode($file,true);
+        dump($file);
+    }
+}
